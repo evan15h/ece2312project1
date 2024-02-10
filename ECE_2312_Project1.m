62b4429d01cd798de9fa759c08aa5b78111d2e65
Evan Haas <159657777+evan15h@users.noreply.github.com>
2024-02-10 15:44:57 -0500

    Update ECE_2312_Project1.m

--- ECE_2312_Project1.m
+++ ECE_2312_Project1.m
@@ -1,10 +1,11 @@
+%Code used to initially record all three phrases using MATLAB
+
 clc;
 
-inputDeviceID = audiodevinfo(1, 44100, 16, 1);
+inputDeviceID = audiodevinfo(1, 44100, 16, 1); %Obtain appropriate ID for the Samsung Book3 audio device
 myRecorder = audiorecorder(44100, 16, 1, inputDeviceID);
 recordblocking(myRecorder, 5);
 
-%Get waveform data
 myRecording = getaudiodata(myRecorder);
-filename = 'ThirdPhrase.wav';
+filename = 'ThirdPhrase.wav'; %'FirstPhrase', 'SecondPhrase', 'ThirdPhrase'...
 audiowrite(filename, myRecording, 44100);
